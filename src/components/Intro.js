cf772f43cad5878bb1486b1674e42c3a1ee4d0cc
atyhamos <atyhalive@gmail.com>
2022-09-11 14:31:19 +0800

    Add navbar

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Intro = () => {
+  return (
+    <div className="container-lg vh-100 d-flex justify-content-center align-items-center position-fixed">
+      <h1 className="appear-disappear">Hi, I'm Van!</h1>
+    </div>
+  )
+}
+
+export default Intro
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,59 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Intro from "../components/intro"
 
 const IndexPage = () => (
-  <div className="container-lg vh-100 d-flex justify-content-center align-items-center">
-    <h1 className="appear">Hi, I'm Van!</h1>
-  </div>
+  <>
+    {}
+    <div className="container-lg vh-100 d-flex justify-content-center align-items-center position-absolute appear-disappear">
+      <h1>Hi, I'm Van!</h1>
+    </div>
+    <main className="appear">
+      <nav class="navbar navbar-expand-md navbar-light display-6">
+        <div class="container-xxl">
+          <a href="#intro" class="nav-link nav-item">
+            van
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#main-nav"
+            aria-controls="main-nav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            class="collapse navbar-collapse justify-content-end align-center"
+            id="main-nav"
+          >
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Illustrations
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Animations
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </main>
+  </>
 )
 
 export const Head = () => (
