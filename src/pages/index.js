69c1eb24e06e42fc882813fed074ddc97deef66b
atyhamos <atyhalive@gmail.com>
2022-09-15 12:32:42 +0800

    Add images to illustration

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,18 +8,17 @@ import Intro from "../components/Intro"
 const IndexPage = () => (
   <>
     {}
-    <div className="container-lg vh-100 d-flex justify-content-center align-items-center position-absolute appear-disappear">
+    <div className="vw-100 vh-100 d-flex justify-content-center align-items-center position-absolute appear-disappear">
       <h1>Hi, I'm Van!</h1>
     </div>
-    <main className="appear">
-      <nav class="navbar navbar-expand-md navbar-light display-6">
-        <div class="container-xxl">
-          <a href="#intro" class="nav-link nav-item">
-            van
-          </a>
-
+    <main className="appear-delay p-4">
+      <nav className="navbar navbar-expand-md navbar-light display-6">
+        <div className="container-xxl d-flex align-items-baseline">
+          <Link to="/" className="nav-link nav-item fw-bold">
+            Van
+          </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#main-nav"
@@ -27,26 +26,31 @@ const IndexPage = () => (
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
 
           <div
-            class="collapse navbar-collapse justify-content-end align-center"
+            className="navbar-collapse justify-content-end align-center"
             id="main-nav"
           >
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Illustrations
+            <ul className="navbar-nav">
+              <li className="nav-item border-end">
+                <Link className="nav-link" href="/illustration">
+                  Illustration
+                </Link>
+              </li>
+              <li className="nav-item border-end">
+                <a className="nav-link" href="animation">
+                  Animation
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Animations
+              <li className="nav-item border-end">
+                <a className="nav-link" href="life">
+                  Life Drawing
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
+              <li className="nav-item">
+                <a className="nav-link" href="about">
                   About
                 </a>
               </li>
