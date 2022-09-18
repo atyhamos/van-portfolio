2475621e660af35fe71a1b1749264197bd15fadc
atyhamos <atyhalive@gmail.com>
2022-09-18 21:53:55 +0800

    Remove gatsby netlify package for SSR

--- src/data/illustrations.js
+++ src/data/illustrations.js
@@ -3,15 +3,15 @@ import i2 from "../images/i-2.png"
 import i3 from "../images/i-3.png"
 import i4 from "../images/i-4.png"
 import i5 from "../images/i-5.png"
-// import i6 from "../images/i-6.png"
-// import i7 from "../images/i-7.jpg"
-// import i8 from "../images/i-8.png"
-// import i9 from "../images/i-9.jpg"
-// import i10 from "../images/i-10.png"
-// import i11 from "../images/i-11.png"
-// import i12 from "../images/i-12.jpg"
-// import i13 from "../images/i-13.png"
-// import i14 from "../images/i-14.png"
+import i6 from "../images/i-6.png"
+import i7 from "../images/i-7.jpg"
+import i8 from "../images/i-8.png"
+import i9 from "../images/i-9.jpg"
+import i10 from "../images/i-10.png"
+import i11 from "../images/i-11.png"
+import i12 from "../images/i-12.jpg"
+import i13 from "../images/i-13.png"
+import i14 from "../images/i-14.png"
 
 export const illustrations = [
   i1,
@@ -19,13 +19,13 @@ export const illustrations = [
   i3,
   i4,
   i5,
-  // i6,
-  // i7,
-  // i8,
-  // i9,
-  // i10,
-  // i11,
-  // i12,
-  // i13,
-  // i14,
+  i6,
+  i7,
+  i8,
+  i9,
+  i10,
+  i11,
+  i12,
+  i13,
+  i14,
 ]
